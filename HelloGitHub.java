e5701a4789e9a6148564308fa68334e58561561f
Carlos Andrews <crdandrews@gmail.com>
2021-03-15 01:49:18 -0500

    Change Printing method to for loop

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -1,7 +1,12 @@
 
 public class HelloGitHub {
 	public static void main(String[] args) {
-		System.out.println("Hello Git Hub");
-		
+		//System.out.println("Hello Git Hub");
+		String str = "Hello GitHub";
+		int i ;
+		for(i = 0; i < str.length(); ++i) {
+			System.out.print(str.charAt(i));
+		}
+
 	}
 }
